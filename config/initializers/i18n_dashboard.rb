ce267ae1766e8e47142a8c7e44829fcbf87fb485
max <mk@kernel-corp.com>
2014-11-10 19:04:32 +0600

    remove unused code

--- config/initializers/i18n_dashboard.rb
+++ config/initializers/i18n_dashboard.rb
@@ -3,10 +3,3 @@ I18nDashboard::Engine.redis = TRANSLATION_STORE
 I18nDashboard.configure do |config|
   config.authentication_method = :authenticate_and_authorize_i18n!
 end
-# module I18nDashboard
-#   class TranslationsController < ::I18nDashboard::ApplicationController
-#     def index
-#       @translations = TRANSLATION_STORE.keys
-#     end
-#   end
-# end
