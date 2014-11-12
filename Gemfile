5148de817d173ea6e64bae861724a239ad306d0e
Zamyatin-AA <stekbox@gmail.com>
2014-11-12 17:39:46 +0600

    user controller

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'cancancan'
 
 # View handles
 gem 'slim-rails'
+gem 'html2slim'
+gem 'compass-rails'
 
 #ember js
 gem 'ember-rails'
@@ -59,6 +61,9 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+
+gem 'gon' # output params from rails -> js
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
@@ -89,7 +94,8 @@ group :development do
 end
 
 group :production do
-  gem 'unicorn'
+     nil?
+  gem 'unicorn', platform: :ruby
 end
 
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Yufu::Application.routes.draw do
 
+
   root to: 'main#index'
 
   mount RailsAdmin::Engine => '/administration', as: 'rails_admin'
   mount I18nDashboard::Engine => '/i18n_dashboard'
 
   devise_for :admin
-  devise_for :users
+  devise_for :user, controllers: { registrations: 'users/registrations', passwords: 'users/passwords' }
 
+  resource :user
 
   namespace :api do
     namespace :v1 do
       resources :languages,  only: [:index, :show]
       resources :directions, only: [:index, :show]
       resources :cities,     only: [:index, :show]
-      resources :users,      only: [:index, :show, :update, :create]
+      resources :user,      only: [:index, :show, :update, :create]
       resources :profiles
     end
   end
