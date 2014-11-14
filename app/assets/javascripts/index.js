0bee748a8238d74ed41117d7ba1aebca185a12d8
zamyatin-aa <stekbox@gmail.com>
2014-11-14 18:54:55 +0600

    Main page transfer still in progress

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+	//Первый ряд блоков
+	//Текущий блок
+	//Первое наведение
+	var firstBlockFirstTime = true;
+	var firstBlockCurrentBlock;
+
+    $("DIV.block.first DIV.item").mouseenter(function() {
+        $(this).css("cursor", "pointer");
+        $(this).find("DIV.over").fadeIn("fast");
+    }).mouseleave(function() {
+        $(this).find("DIV.over").fadeOut("fast");
+    });
+
+    $("DIV.block.first DIV.item").click(function(){
+        scrollToDiv($("DIV.block DIV.item P.h2"), 20);
+        var target = $(this).data("target");
+        $("DIV.first-points DIV.container").hide();
+        $("DIV.first-points").show();
+        $("DIV.first-points DIV.container." + target).show();
+    });
+/*	$("DIV.block.first DIV.item").mouseenter(function() {
+		//Скроллинг к нужному месту на странице
+		scrollToDiv($("DIV.block.first"), 0);
+		//Выбранный блок
+		var target = $(this).data("target");
+		//При повторном наведении на тот же блок ничего делать не будем
+		if(firstBlockCurrentBlock != target) {
+			//Прячем все оверы
+			$("DIV.block.first DIV.item DIV.over").fadeOut();
+			//Отображаем нужный овер
+			$(this).find("DIV.over").fadeIn();
+			//Отобразим пункты выбранного блока
+			//Если в первый раз
+			if(firstBlockFirstTime) {
+				$("DIV.first-points DIV.container." + target).show();
+				$("DIV.first-points").slideDown();
+				firstBlockFirstTime = false;
+			} else {
+				$("DIV.first-points DIV.container").fadeOut();
+				$("DIV.first-points DIV.container." + target).fadeIn();
+			}
+			//Меняем текущий блок
+			firstBlockCurrentBlock = target;
+		}//if
+	});//mouseenter*/
+	//Оверы первого блока
+	$("DIV.first-points DIV.item").hover(function() {
+		$(this).addClass("active");
+		$(this).find("DIV.over").fadeIn();
+	}, function() {
+		$(this).removeClass("active");
+		$(this).find("DIV.over").fadeOut();
+	});//hover
+	//Прячем пункты первого блока
+	$("DIV.first-points").hide();
+	$("DIV.first-points DIV.container").hide();
+	//Спрчем оверы
+	$("DIV.first-points DIV.over").hide();
+});//ready
