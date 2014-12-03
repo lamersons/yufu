1c425a11c6a45aa0185931d62ba3b6a37d9bd0e5
max <mk@kernel-corp.com>
2014-12-03 12:06:32 +0600

    validates presents of password

--- app/models/virtual/user.rb
+++ app/models/virtual/user.rb
@@ -1,7 +1,8 @@
 class Virtual::User < ActiveRecord::Base
   belongs_to :domain, class_name: 'Virtual::Domain'
 
-  before_save :encrypt_password, if: :password_changed?
+  before_save :encrypt_password, if: -> { password.present? && password_changed?}
+  validates_presence_of :password
 
   private
   def encrypt_password
