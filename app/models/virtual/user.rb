a1210b538a58edb043ca163905050041dbe59ce2
max <mk@kernel-corp.com>
2014-12-03 11:50:21 +0600

    encrypt password

--- app/models/virtual/user.rb
+++ app/models/virtual/user.rb
@@ -1,3 +1,10 @@
 class Virtual::User < ActiveRecord::Base
   belongs_to :domain, class_name: 'Virtual::Domain'
+
+  before_save :encrypt_password, if: :password_changed?
+
+  private
+  def encrypt_password
+    self.password = Digest::SHA1.new(512).digest password
+  end
 end
