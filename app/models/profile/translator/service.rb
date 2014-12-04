09dd9f034b2fe8d421871e8428d38751563a95d6
max <mk@kernel-corp.com>
2014-12-04 20:48:23 +0600

    fix edit profiles

--- app/models/profile/translator/service.rb
+++ app/models/profile/translator/service.rb
@@ -4,14 +4,19 @@ module Profile
       include Mongoid::Document
 
       field :level
-      field :has_hsk,       type: Boolean
+      field :has_hsk,       type: Mongoid::Boolean
       field :verbal_price,  type: Float
       field :written_price, type: Float
       field :written_translate_type
+      field :is_approved,   type: Mongoid::Boolean
 
       belongs_to :language
 
       embedded_in :profile_translator_individual
+
+      def name
+        "#{language.name} | lvl: #{level}"
+      end
     end
   end
 end
