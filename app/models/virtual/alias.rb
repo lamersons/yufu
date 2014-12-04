09805965b651cd8afd2a0143c03619a81d084ce1
max <mk@kernel-corp.com>
2014-12-04 12:14:54 +0600

    validate presents of domain

--- app/models/virtual/alias.rb
+++ app/models/virtual/alias.rb
@@ -1,3 +1,4 @@
 class Virtual::Alias < ActiveRecord::Base
   belongs_to :domain, class_name: 'Virtual::Domain'
+  validates_presence_of :domain
 end
--- app/models/virtual/user.rb
+++ app/models/virtual/user.rb
@@ -2,7 +2,8 @@ class Virtual::User < ActiveRecord::Base
   belongs_to :domain, class_name: 'Virtual::Domain'
 
   before_save :encrypt_password, if: -> { password.present? && password_changed?}
-  validates_presence_of :password
+  validates_presence_of :password, :domain
+
 
   private
   def encrypt_password
