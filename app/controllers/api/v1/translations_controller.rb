a0978b7d97221a50eea32067d52a8c68de9876ae
max <mk@kernel-corp.com>
2014-12-09 18:29:30 +0600

    translation in progress

--- app/controllers/api/v1/translations_controller.rb
+++ app/controllers/api/v1/translations_controller.rb
@@ -5,10 +5,13 @@ module Api
 
       def index
         @translations = I18nDashboard::Translation.all
+        original_locale = params[:original_locale].blank? ? I18n.locale : params[:original_locale]
         respond_to do |formats|
           formats.json do
             objects = []
-            @translations.each { |tr| objects << {id: tr}}
+            @translations.each do |tr|
+              objects << {id: tr, value: t(tr, default: ''), original: t(tr, locale: original_locale) }
+            end
             render json: {translations: objects}
           end
         end
