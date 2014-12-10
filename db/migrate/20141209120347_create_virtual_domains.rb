1f1c607f38e2968f23a8ad79f0c2be1a6baf24ca
mitchenkod <mitchenko1897@mail.ru>
2014-12-10 19:41:17 +0600

    order verbal feature

--- db/migrate/20141209120347_create_virtual_domains.rb
+++
@@ -1,9 +0,0 @@
-class CreateVirtualDomains < ActiveRecord::Migration
-  def change
-    if ENV['RAILS_ENV'] != 'production'
-      create_table :virtual_domains do |t|
-        t.string :name
-      end
-    end
-  end
-end
