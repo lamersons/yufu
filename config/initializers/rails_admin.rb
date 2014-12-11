feb5a61c582b6d5d4261b4fb0c0c378254b5dc9c
max-konin <mk@kernel-corp.com>
2014-12-11 19:12:25 +0600

    fix specs

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -52,18 +52,4 @@ RailsAdmin.config do |config|
       enum { %w(I18n User City Direction Language) }
     end
   end
-
-  config.model Profile::Base do
-    field :user do
-      nested_form false
-    end
-    include_all_fields
-  end
-
-  config.model Profile::Translator::Base do
-    field :user do
-      nested_form false
-    end
-    include_all_fields
-  end
 end
