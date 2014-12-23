240dcd3a36ad89055ee33d091a88c999369c3f61
max-konin <mk@kernel-corp.com>
2014-12-23 15:23:58 +0600

    export i18n-js

--- app/controllers/api/v1/translations_controller.rb
+++ app/controllers/api/v1/translations_controller.rb
@@ -13,6 +13,8 @@ module Api
       def update
         target_locale = params[:translation][:locale]
         I18n.backend.store_translations(target_locale, {params[:id] => params[:translation][:value]}, :escape => false)
+        # TODO: need using delayed jobs (sidekiq vs resque)
+        I18n::JS.export
         render json: { id: params[:id],  value: params[:translation][:value], original: t(params[:id])}
       end
     end
