ec3d87d4e917fd6f63baf94576678b1554a197bf
max-konin <mk@kernel-corp.com>
2014-12-24 16:23:16 +0600

    move city id to translator serializer

--- app/serializers/profile/base_serializer.rb
+++ app/serializers/profile/base_serializer.rb
@@ -1,14 +1,6 @@
 class Profile::BaseSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :middle_name, :phone, :additional_phone, :_type, :created_at, :updated_at,
-             :type, :total_approve, :is_translator, :city_id
-
-  # def links
-  #   { orders: Rails.application.routes.url_helpers.api_v1_profile_orders_url(object) }
-  # end
-
-  def city_id
-    object.city.try(:id).to_s
-  end
+             :type, :total_approve, :is_translator
 
   def type
     object.class.name.gsub '::', ''
--- app/serializers/profile/translator/base_serializer.rb
+++ app/serializers/profile/translator/base_serializer.rb
@@ -1,3 +1,8 @@
 class Profile::Translator::BaseSerializer < Profile::BaseSerializer
-  attributes :skype, :qq, :is_active
+  attributes :skype, :qq, :is_active, :city_id
+
+
+  def city_id
+    object.city.try(:id).to_s
+  end
 end
