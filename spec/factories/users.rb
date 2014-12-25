d9e8a0e74b2fe14d3d7561264275d56ea3956aca
max-konin <mk@kernel-corp.com>
2014-12-25 14:59:45 +0600

    fix ckeditor

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
   end
 
   factory :translator, class: User do
-    sequence(:email) {|n| "user#{100 + n}@example.com"}
+    sequence(:email) {|n| "translator#{100 + n}@example.com"}
     password 'password'
     profiles {[build(:profile_translator_individual)]}
   end
